94e3875110d72a7d4bee11de1c47cf7320ac945d
carlosgarciaopenwebinar <carlosgarciaopenwebinar@gmail.com>
2019-08-14 21:57:29 +0200

    Colocamos en app.component el componente lista para verlo mientras lo editamos

--- src/app/Components/lista-correos/lista-correos.component.ts
+++ src/app/Components/lista-correos/lista-correos.component.ts
@@ -30,6 +30,23 @@ export class ListaCorreosComponent implements OnInit {
     this.correos = [];
     this.correos.push(correo1);
     this.correos.push(correo2);
+
+    this.correos.push({
+      titulo: "Titulo del 3",
+      cuerpo: `Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email
+        Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuer`,
+      emisor: 'correoEmisor3@openWebinar.inv',
+      destinatario: 'correoReceptor@openWebinar.inv',
+      leido: false,
+    });
+    this.correos.push({
+      titulo: "Titulo del 4",
+      cuerpo: `Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email
+        Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuer`,
+      emisor: 'correoEmisor4@openWebinar.inv',
+      destinatario: 'correoReceptor@openWebinar.inv',
+      leido: false,
+    });
   }
 
   ngOnInit() {
