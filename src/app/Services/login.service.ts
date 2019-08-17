d37c2ff1c8dddb6e4e9e1ce035eed1fb7bcb03f7
carlosgarciaopenwebinar <carlosgarciaopenwebinar@gmail.com>
2019-08-18 00:01:27 +0200

    Configuramos el servicio de login para realizar el registro y la desconexion, ademas de guardar y recuperar el usuario del sessionStorage

--- src/app/Services/login.service.ts
+++ src/app/Services/login.service.ts
@@ -1,9 +1,76 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from "@angular/core";
+import * as _ from "lodash";
+import { GoogleAuthService } from "ng-gapi/lib/GoogleAuthService";
+import GoogleUser = gapi.auth2.GoogleUser;
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  public static readonly SESSION_STORAGE_KEY: string = "usuarioGoogle";
 
-  constructor() { }
-}
+  user: GoogleUser = undefined;
+
+  constructor(private googleAuthService: GoogleAuthService, private ngZone: NgZone) { 
+    if(this.isUserSignedIn()){
+      this.user = this.getSessionUser();
+    }
+  }
+
+  public getUser(){
+    return this.user;
+  }
+
+  public getToken(){
+    return this.user.getAuthResponse().access_token;
+  }
+
+  public getUserId(){
+    return this.user.getBasicProfile().getId();
+  }
+
+  private getSessionUser(): GoogleUser {
+    let user: string = sessionStorage.getItem(LoginService.SESSION_STORAGE_KEY);
+    if (!user) {
+      throw new Error("no token set , authentication required");
+    }
+    return JSON.parse(user);
+  }
+
+  public signIn() {
+    this.googleAuthService.getAuth().subscribe((auth) => {
+      auth.signIn().then(
+        res => this.signInSuccessHandler(res),
+        err => this.signInErrorHandler(err));
+    });
+  }
+
+  public signOut(): void {
+    this.googleAuthService.getAuth().subscribe((auth) => {
+      try {
+        auth.signOut();
+        this.user = undefined;
+      } catch (e) {
+        console.error(e);
+      }
+      sessionStorage.removeItem(LoginService.SESSION_STORAGE_KEY);
+    });
+  }
+
+  public isUserSignedIn(): boolean {
+    return !_.isEmpty(sessionStorage.getItem(LoginService.SESSION_STORAGE_KEY));
+  }
+
+  private signInSuccessHandler(res: GoogleUser) {
+    this.ngZone.run(() => {
+      this.user = res;
+      sessionStorage.setItem(
+        LoginService.SESSION_STORAGE_KEY, JSON.stringify(res)
+      );
+    });
+  }
+
+  private signInErrorHandler(err) {
+    console.warn(err);
+  }
+} 
