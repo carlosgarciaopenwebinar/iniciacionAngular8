4b2fbacc65ca3eae0726d016ad8a90d4c265a588
carlosgarciaopenwebinar <carlosgarciaopenwebinar@gmail.com>
2019-08-18 02:13:00 +0200

    Registramos las rutas en el fichero app-routing e incluimos en app.component solo el menu y el componente para rutas router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './Views/home/home.component';
+import { CorreosRecibidosComponent } from './Views/correos-recibidos/correos-recibidos.component';
+import { EnviarComponent } from './Views/enviar/enviar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'mail', component: CorreosRecibidosComponent },
+  { path: 'send', component: EnviarComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
